0ddbdddc3e38b518a58c79a7faf0131dccb8935c
Guilherme dos Reis <greis.gnr@gmail.com>
2018-05-15 07:46:35 -0300

    Import StartButton style and pass the class as prop

--- src/components/StartButton.js
+++ src/components/StartButton.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+import './StartButton.css'
 import Button from './Button'
 
 const StartButton = ({ isSpinning, handleStartClick }) => (
   <Button
+    className="btn-start"
     handleClick={handleStartClick}
     isDisabled={isSpinning}
   >Start</Button>
