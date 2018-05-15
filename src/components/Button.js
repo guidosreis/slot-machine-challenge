a807f1fac04478ee8b52cf0f7d8e310c81d47001
Guilherme dos Reis <greis.gnr@gmail.com>
2018-05-15 07:44:50 -0300

    Add className prop to Button

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,14 +1,16 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const Button = ({ children, isDisabled, handleClick }) => (
+const Button = ({ children, className, isDisabled, handleClick }) => (
   <button 
+    className={className}
     disabled={isDisabled}
     onClick={handleClick}
   >{children}</button>
 )
 
 Button.propTypes = {
+  className: PropTypes.string.isRequired,
   isDisabled: PropTypes.bool.isRequired,
   handleClick: PropTypes.func.isRequired
 }
